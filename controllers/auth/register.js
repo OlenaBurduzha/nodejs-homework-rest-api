ac639d04cb46dc08c6312da1692d22c55e56c6eb
OlenaBurduzha <maglerb86@gmail.com>
2023-04-11 15:43:38 +0300

    add hw06-email

--- controllers/auth/register.js
+++ controllers/auth/register.js
@@ -1,6 +1,8 @@
 const { Conflict } = require("http-errors");
 const gravatar = require("gravatar");
+const { nanoid } = require("nanoid");
 
+const { sendEmail } = require("../../helpers");
 const { User } = require("../../models");
 
 const register = async (req, res) => {
@@ -9,12 +11,20 @@ const register = async (req, res) => {
   if (user) {
     throw new Conflict(`User with email: ${email} already exist`);
   }
+
+  const verificationToken = nanoid();
   const avatarURL = gravatar.url(email);
-  const newUser = new User({ email, avatarURL });
+  const newUser = new User({ email, avatarURL, verificationToken });
 
   newUser.setPassword(password);
 
-  newUser.save();
+  await newUser.save();
+  const mail = {
+    to: email,
+    subject: "Підтвердження email",
+    html: `<a target="_blank" href="http://localhost:3000/api/users/verify/${verificationToken}"> Підтвердити email</a>`,
+  };
+  await sendEmail(mail);
 
   res.status(201).json({
     status: "success",
@@ -24,9 +34,9 @@ const register = async (req, res) => {
       user: {
         email,
         avatarURL,
+        verificationToken,
       },
     },
   });
 };
-
 module.exports = register;
